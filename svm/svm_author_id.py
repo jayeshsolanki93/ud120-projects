38a42dd0eeae101c2863780a301895c69df1565d
Jayesh Solanki <jayeshsolanki93@gmail.com>
2020-05-24 23:18:29 +0200

    Complete SVM classifier exercise

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,12 +19,26 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn.svm import SVC
 
+clf = SVC(kernel='rbf', C = 10000.0)
 
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
-#########################################################
-### your code goes here ###
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
 
-#########################################################
+t1 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time() - t1, 3), "s"
 
+print clf.score(features_test, labels_test)
 
+count = 0
+for p in pred:
+    if p == 1:
+        count += 1
+
+print count
